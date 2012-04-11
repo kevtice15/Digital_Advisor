c4fc0db56921cc97f7e6e2ed5c1d3bcc61d6fa41
KeVon Ticer <kevtice15@gmail.com>
2012-04-10 23:11:35 -0400

    Added Initial Code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Finaldigi::Application.config.secret_token = '03a0138dd7b3ae58abb430dcb0be9fc26480ba469ba4d7b3f1af8cf36d3abaa5af3b8b6c6c1be2f8eac9c16e0e28a69c4ff248a426a4cc21aa297941f2c607fe'
